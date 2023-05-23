43a8d01a70e225b4055340eb0b2c57ef4fafb8ed
Deniz Gur <dnzgur@gmail.com>
2023-05-23 17:13:51 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '5a04250b61624cea8f1540652b51858e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'OP Digital Mobile Growth Consultancy',
+  domain: 'op.digital',
+  author: 'Deniz Gur',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
