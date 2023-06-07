b0bded8f46f8da232a580da960da434e542193ef
Deniz Gur <dnzgur@gmail.com>
2023-06-07 11:55:19 +0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -18,7 +18,7 @@ export default siteConfig({
 
   // social usernames (optional)
   twitter: 'opdenizgur',
-  linkedin: 'company/op-mobile',
+  linkedin: 'company/opdigitalagency',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
